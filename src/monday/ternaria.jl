750dd4b59d00453875613e4869c1e5201a9c5ac0
andres salinas <abrandsal@gmail.com>
2023-09-24 11:23:17 -0300

    added ternaria.jl

---
+++ src/monday/ternaria.jl
@@ -0,0 +1,33 @@
+using CSV,  DataFrames
+
+periodo_anterior(x::Integer) =  x % 100 > 1  ?  x-1  : 12 + (div(x,100) -1) * 100
+
+df = CSV.read("competencia_01_crudo.csv", DataFrame)
+sort!(df, [:numero_de_cliente, :foto_mes])
+
+global periodo_ultimo = maximum( df.foto_mes )
+global periodo_anteultimo = periodo_anterior( periodo_ultimo)
+
+# assign most common class values
+df.clase_ternaria = @. ifelse( df.foto_mes < periodo_anteultimo, "CONTINUA", missing )
+
+# pre compute sequential time
+periodo = @. div(df.foto_mes,100)*12 + df.foto_mes%100
+
+global last = nrow(df)
+
+for i in 1:last
+  if df.foto_mes[i] <= periodo_anteultimo &&  i < last && df.numero_de_cliente[i] != df.numero_de_cliente[i+1]
+          df.clase_ternaria[i] = "BAJA+1"
+  end
+
+  if df.foto_mes[i] < periodo_anteultimo &&  i+1 < last && df.numero_de_cliente[i] == df.numero_de_cliente[i+1] &&  periodo[i+1] == periodo[i] +1 &&
+        ( df.numero_de_cliente[i+1] != df.numero_de_cliente[i+2]  || df.numero_de_cliente[i+1] == df.numero_de_cliente[i+2] && periodo[i+2]  > periodo[i+1] +2)
+          df.clase_ternaria[i] = "BAJA+2"
+  end
+end
+
+CSV.write( "competencia_02.csv", df )
+
+
+
