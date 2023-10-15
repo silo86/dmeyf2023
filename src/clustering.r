44c6c31ae17d640f8bcef37a41e2ec77c39c6a0c
andres salinas <abrandsal@gmail.com>
2023-10-14 22:28:33 -0300

    new code clustering

--- src/clustering.r
+++ src/clustering.r
@@ -8,13 +8,15 @@
 # limpio la memoria
 rm(list = ls()) # remove all objects
 gc() # garbage collection
-
+#install.packages("ggplot2")
+library("ggplot2")
 require("data.table")
 require("lightgbm")
 library(dplyr)
 library(randomForest)
 library(dendextend)
-
+library(data.table)
+library(lubridate)
 
 PARAM <- list()
 PARAM$experimento <- "clustering_dataset"
@@ -39,8 +41,10 @@ PARAM$finalmodel$max_bin <- 31
 #------------------------------------------------------------------------------
 #------------------------------------------------------------------------------
 # Aqui empieza el programa
-#setwd("~/buckets/b1")
-setwd("/Users/andres/Desktop/master/DM_EyF")
+setwd("~/buckets/b1")
+#setwd("/Users/andres/Desktop/master/DM_EyF")
+
+# cargo el dataset donde voy a entrenar
 
 # cargo el dataset donde voy a entrenar
 
@@ -49,62 +53,230 @@ subset <- dataset[dataset$clase_ternaria == "BAJA+2", ]
 subset <- subset[, !("clase_ternaria"), with = FALSE]
 subset <- subset[!duplicated(subset$numero_de_cliente), ]
 # Convierte las columnas de fecha a objetos Date
-dataset$foto_mes <- as.Date(dataset$foto_mes, format = "%Y-%m")
-subset$foto_mes <- as.Date(subset$foto_mes, format = "%Y-%m")
-
-dataset_2 <- dataset %>%
-  filter(numero_de_cliente %in% subset$numero_de_cliente & foto_mes <= subset$foto_mes)
 
 
+dataset$foto_mes <- as.Date(paste0(dataset$foto_mes, "01"), format = "%Y%m%d")
+subset$foto_mes <- as.Date(paste0(subset$foto_mes, "01"), format = "%Y%m%d")
 
-# Filtra los registros de dataset1 que cumplen con la condición
-#dataset_2 <- subset(dataset, numero_de_cliente %in% subset$numero_de_cliente) #& foto_mes <= subset$foto_mes)
+
+#Convertir a data.table
+subset <- data.table(subset)
+dataset <- data.table(dataset)
+
+dataset_2 <- dataset[dataset$numero_de_cliente %in% subset$numero_de_cliente & dataset$foto_mes <= subset$foto_mes]
 
 
-# Identify numeric columns
-numeric_columns <- names(subset)[sapply(subset, is.numeric)]
 
-# Replace missing values with the median for numeric columns
-for (col in numeric_columns) {
-  subset[is.na(subset[[col]]), (col) := median(subset[[col]], na.rm = TRUE)]
+
+# Calculo la columna relative_month
+dataset_2 <- dataset_2 %>%
+  arrange(numero_de_cliente, foto_mes) %>%
+  group_by(numero_de_cliente) %>%
+  mutate(relative_month = as.numeric(interval(max(foto_mes), foto_mes) / months(1)))
+
+
+######## Test#############
+# dataset_2 %>%
+#   filter(numero_de_cliente == 29199686) %>%
+#   select(foto_mes, relative_month,clase_ternaria)
+######## Test#############
+
+# Escribo dataset_2
+fwrite(dataset_2, file = "dataset_2.csv", sep = ",")
+
+if (!exists("dataset_1")) {
+  # Intenta leer dataset_1 desde el archivo CSV
+  dataset_1 <- fread('dataset_1.csv', stringsAsFactors = TRUE)
 }
 
-# Identify character columns
-character_columns <- names(subset)[sapply(subset, is.character)]
-
-# Impute missing values with random sampling from existing values for character columns
-#library(dplyr)
-
-# Impute missing values in character columns
-
-for (col in character_columns) {
-  # Find the non-missing values in the column
-  non_missing_values <- na.omit(subset$col)
-  
-  # If there are missing values in the column
-  if (length(non_missing_values) > 0) {
-    # Impute the missing values with a random sample of the non-missing values
-    imputed_values <- sample_frac(non_missing_values, 1, replace = TRUE)
-    
-    # Mutate the subset to replace the missing values
-    subset <- mutate(subset, col = replace_na(col, imputed_values))
-  }
+if (!exists("dataset_1")) {
+  # Si dataset_1 todavía no existe, ejecuta el código para crearlo
+  subset$foto_mes <- as.numeric(subset$foto_mes)
+  subset <- na.roughfix(subset)
+  rf.fit <- randomForest(x = subset, y = NULL, ntree = 1000, proximity = TRUE, oob.prox = TRUE)
+  hclust.rf <- hclust(as.dist(1 - rf.fit$proximity), method = "ward.D2")
+  rf.cluster <- cutree(hclust.rf, k = 7)
+  dataset_1 <- cbind(subset, rf.cluster)
+  # Escribo dataset_1
+  fwrite(dataset_1, file = "dataset_1.csv", sep = ",")
 }
 
 
-# Aplico Random Forest para clustering
-
-rf.fit <- randomForest(x = subset, y = NULL, ntree = 1000, proximity = TRUE, oob.prox = TRUE)
-hclust.rf <- hclust(as.dist(1-rf.fit$proximity), method = "ward.D2")
-rf.cluster = cutree(hclust.rf, k=7)
-# merge with subset
-dataset_1 <- cbind(subset, rf.cluster)
+merged_dataset <- merge(dataset_2, dataset_1[, c("numero_de_cliente", "rf.cluster")], by.x = "numero_de_cliente", by.y = "numero_de_cliente", all.x = TRUE)
 
 
-fwrite(dataset_1, file = "dataset_1.csv", sep = ",")
 
-result <- aggregate(. ~ rf.cluster, data = dataset_1, FUN = median)
-result_table <- dcast(result, rf.cluster ~ variable_column_name)
-result_table <- t(result_table)
+#result <- aggregate(. ~ rf.cluster, data = merged_dataset, FUN = mean)
+
+
+#ggplot(merged_dataset, aes(relative_month, Master_delinquency, color = factor(rf.cluster))) +
+#  geom_smooth()
+
+#library(gridExtra)
+
+# Crear un vector con los nombres de las columnas que deseas graficar (excepto 'relative_month')
+#columns_to_plot <- colnames(merged_dataset)[-which(colnames(merged_dataset) == 'relative_month')]
+#columns_to_plot <- c("Master_delinquency","Visa_mpagominimo")
+columns_to_plot <- setdiff(
+  colnames(merged_dataset),
+  c("numero_de_cliente", "foto_mes", "clase_ternaria", "rf.clusters", "clase_ternaria","relative_month")
+)
+
+# Inicializar una lista para almacenar las gráficas
+plot_list <- list()
+
+# Crear las gráficas y agregarlas a la lista
+for (col in columns_to_plot) {
+  p <- ggplot(merged_dataset, aes(relative_month, .data[[col]], color = factor(rf.cluster))) +
+    geom_smooth() +
+    labs(title = col)
+  plot_list[[col]] <- p
+}
+
+# Guardar las gráficas en un archivo PDF
+pdf("merged_dataset_plots.pdf", width = 8, height = 5)
+do.call(grid.arrange, plot_list)
+dev.off()
+
+
+
+
+###### VERSION CLOUD
+
+# para correr el Google Cloud
+#   8 vCPU
+#  16 GB memoria RAM
+# my_seeds : 100019,100043,100049,100057,100069
+
+
+
+# limpio la memoria
+rm(list = ls()) # remove all objects
+gc() # garbage collection
+#install.packages("ggplot2")
+library("ggplot2")
+require("data.table")
+require("lightgbm")
+library(dplyr)
+library(randomForest)
+library(dendextend)
+library(data.table)
+library(lubridate)
+
+PARAM <- list()
+PARAM$experimento <- "clustering_dataset"
+
+PARAM$input$dataset <- "./datasets/competencia_02.csv.gz"
+
+
+PARAM$input$training <- c(202101, 202102, 202103, 202104, 202105) # meses donde se entrena el modelo
+PARAM$input$future <- c(202107) # meses donde se aplica el modelo
+
+PARAM$finalmodel$semilla <- 100019
+
+PARAM$finalmodel$num_iterations <- 4928
+PARAM$finalmodel$learning_rate <- 0.0189943331895954
+PARAM$finalmodel$feature_fraction <- 0.892623977897483
+PARAM$finalmodel$min_data_in_leaf <- 785
+PARAM$finalmodel$num_leaves <- 666
+
+
+PARAM$finalmodel$max_bin <- 31
+
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+# Aqui empieza el programa
+setwd("~/buckets/b1")
+#setwd("/Users/andres/Desktop/master/DM_EyF")
+
+# cargo el dataset donde voy a entrenar
+
+# cargo el dataset donde voy a entrenar
+
+dataset <- fread(PARAM$input$dataset, stringsAsFactors = TRUE)
+subset <- dataset[dataset$clase_ternaria == "BAJA+2", ]
+subset <- subset[, !("clase_ternaria"), with = FALSE]
+subset <- subset[!duplicated(subset$numero_de_cliente), ]
+# Convierte las columnas de fecha a objetos Date
+
+
+dataset$foto_mes <- as.Date(paste0(dataset$foto_mes, "01"), format = "%Y%m%d")
+subset$foto_mes <- as.Date(paste0(subset$foto_mes, "01"), format = "%Y%m%d")
+
+
+#Convertir a data.table
+subset <- data.table(subset)
+dataset <- data.table(dataset)
+
+dataset_2 <- dataset[dataset$numero_de_cliente %in% subset$numero_de_cliente & dataset$foto_mes <= subset$foto_mes]
+
+
+
+
+# Calculo la columna relative_month
+dataset_2 <- dataset_2 %>%
+  arrange(numero_de_cliente, foto_mes) %>%
+  group_by(numero_de_cliente) %>%
+  mutate(relative_month = as.numeric(interval(max(foto_mes), foto_mes) / months(1)))
+
+
+######## Test#############
+# dataset_2 %>%
+#   filter(numero_de_cliente == 29199686) %>%
+#   select(foto_mes, relative_month,clase_ternaria)
+######## Test#############
+
+# Escribo dataset_2
+fwrite(dataset_2, file = "dataset_2.csv", sep = ",")
+
+if (!exists("dataset_1")) {
+  # Intenta leer dataset_1 desde el archivo CSV
+  dataset_1 <- fread('dataset_1.csv', stringsAsFactors = TRUE)
+}
+
+if (!exists("dataset_1")) {
+  # Si dataset_1 todavía no existe, ejecuta el código para crearlo
+  subset$foto_mes <- as.numeric(subset$foto_mes)
+  subset <- na.roughfix(subset)
+  rf.fit <- randomForest(x = subset, y = NULL, ntree = 1000, proximity = TRUE, oob.prox = TRUE)
+  hclust.rf <- hclust(as.dist(1 - rf.fit$proximity), method = "ward.D2")
+  rf.cluster <- cutree(hclust.rf, k = 7)
+  dataset_1 <- cbind(subset, rf.cluster)
+  # Escribo dataset_1
+  fwrite(dataset_1, file = "dataset_1.csv", sep = ",")
+}
+
+
+merged_dataset <- merge(dataset_2, dataset_1[, c("numero_de_cliente", "rf.cluster")], by.x = "numero_de_cliente", by.y = "numero_de_cliente", all.x = TRUE)
+
+
+
+#result <- aggregate(. ~ rf.cluster, data = merged_dataset, FUN = mean)
+
+
+#ggplot(merged_dataset, aes(relative_month, Master_delinquency, color = factor(rf.cluster))) +
+#  geom_smooth()
+
+# Obtén las columnas a graficar
+columns_to_plot <- setdiff(
+  colnames(merged_dataset),
+  c("numero_de_cliente", "foto_mes", "clase_ternaria", "rf.clusters", "clase_ternaria", "relative_month")
+)
+
+# Inicializa el archivo PDF
+pdf("merged_dataset_plots.pdf", width = 8, height = 5)
+
+# Crea y guarda un gráfico en cada página
+for (col in columns_to_plot) {
+  p <- ggplot(merged_dataset, aes(relative_month, .data[[col]], color = factor(rf.cluster))) +
+    geom_smooth() +
+    labs(title = col)
+  print(p)  # Imprime el gráfico en el PDF
+  cat("\n\n")  # Añade una página en blanco
+}
+
+# Cierra el archivo PDF
+dev.off()
+
 
 
