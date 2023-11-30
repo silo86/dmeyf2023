2b8c7cc1fc706b125560332fa7ae3116a4362759
andres salinas <abrandsal@gmail.com>
2023-11-30 10:31:51 -0300

    final

--- src/kaggle 3/lightGBM_ensemble_final.r
+++ src/kaggle 3/lightGBM_ensemble_final.r
@@ -1,5 +1,5 @@
 # Acá evaluar y escribir recomendación de recursos
-
+# my_seeds : 100019,100043,100049,100057,100069
 # Limpio la memoria
 rm(list = ls()) # remuevo todos los objetos
 gc() # garbage collection
@@ -7,7 +7,6 @@ gc() # garbage collection
 require("data.table")
 require("lightgbm")
 
-#-----------------------------------CONFIGURAR PARÁMETROS-------------------------------------------#
 # Defino los parametros de la corrida, en una lista, la variable global  PARAM
 PARAM <- list()
 
@@ -25,10 +24,8 @@ PARAM$input$training <- c( 201901, 201902, 201903, 201904, 201905, 202906,
 PARAM$input$future <- c(202109)
 
 # Defino parámetros:
-
-# Parámetro variable (esto genera semillas con valor entre 15k y 80k, puede ajustar a preferencia)
-cantidad_semillas = 10 # Cuántas semillas desea ensamblar?
-semillas <- as.integer(seq(10000, 80000, length.out = cantidad_semillas))
+cantidad_semillas = 10 
+semillas <- as.integer(seq(10000, 100000, length.out = cantidad_semillas))
 
 # Parámetros fijos obtenidos en la Optimización Bayesiana 
 PARAM$finalmodel$num_iterations <- 675
@@ -38,7 +35,6 @@ PARAM$finalmodel$min_data_in_leaf <- 6221
 PARAM$finalmodel$num_leaves <- 433
 PARAM$finalmodel$max_bin <- 33
 
-# *********************************************Cargo los datos *********************************************
 # Aqui empieza el programa que voy a ejecutar para cada semilla
 # Directorio de origen
 setwd("~/buckets/b1/")
@@ -49,9 +45,6 @@ dataset <- fread(PARAM$input$dataset, stringsAsFactors = TRUE)
 # Configuro la variable target como binaria
 # El criterio: POS = { BAJA+1, BAJA+2 }, NEG {CONTINUA}
 dataset[, clase01 := ifelse(clase_ternaria %in% c("BAJA+2", "BAJA+1"), 1L, 0L)]
-
-#*********************************************SELECCIONAR DATOS*********************************************
-# Campos a utilizar
 campos_buenos <- setdiff(colnames(dataset), c("clase_ternaria", "clase01"))
 
 # Establezco qué datos usaré para entrenar
@@ -61,38 +54,18 @@ dataset[, train := 0L]
 # Asigno un 1 a todas las filas correspondiente al foto_mes configurado en los parámetros de entrada
 dataset[foto_mes %in% PARAM$input$training, train := 1L]
 
-#*********************************************CREAR DIRECTORIOS*********************************************
-# Creo carpeta donde guardar los experimentos en caso de que no exista
 dir.create("./exp/", showWarnings = FALSE)
-
-# Creo carpeta donde guardar este experimento en caso de que no exista
 dir.create(paste0("./exp/", PARAM$experimento, "/"), showWarnings = FALSE)
-
-# Establezco el Working Directory de este experimento
 setwd(paste0("./exp/", PARAM$experimento, "/"))
-
-
-#*********************************************CONFIGURAR DATOS DE ENTRADA MODELO*********************************************
 # Dejo los datos en el formato que necesita LightGBM
 dtrain <- lgb.Dataset(
   data = data.matrix(dataset[train == 1L, campos_buenos, with = FALSE]),
   label = dataset[train == 1L, clase01]
 )
 
-#*********************************************ITERACIÓN*********************************************
-
-# Obtengo los datos a predecir
 dapply <- dataset[foto_mes == PARAM$input$future]
-
-# Selecciono columna con numero de cliente y foto mes en df para guardar las predicciones
 predicciones <- dapply[, list(numero_de_cliente, foto_mes)]
-
-cat("\n\nEmpieza la iteración, hora:", Sys.time(), "\n")
-
 for (semilla in semillas) {
-  #*********************************************CONFIGURAR MODELO*********************************************
-  # Utilizo los parámetros configurados al inicio para el modelo
-
   modelo <- lgb.train(
   data = dtrain,
   param = list(
@@ -107,21 +80,7 @@ for (semilla in semillas) {
   )
   )
 
-  #*********************************************PERSISTIR IMPORTANCIA DE VARIABLES*********************************************
-  # Este paso guarda la importancia de variables de cada modelo generado, puede descomentar si desea guardarlas)
-  # Calculo la importancia de variables del modelo
-  # tb_importancia <- as.data.table(lgb.importance(modelo))
 
-  # Configuro nombre del archivo
-  # archivo_importancia <- paste0("impo_", semilla, ".txt")
-
-  # Guardo en el archivo 
-  # fwrite(tb_importancia,
-  # file = archivo_importancia,
-  # sep = "\t"
-  #)
-
-  #*********************************************PREDECIR SOBRE MES DE INTERÉS*********************************************
   # Aplico el modelo a los nuevos datos
   prediccion <- predict(
   modelo,
@@ -135,23 +94,15 @@ for (semilla in semillas) {
  
 }
 
-#*********************************************PERSISTO SALIDA CON LAS PREDICCIONES DE CADA SEMILLA*********************************************
-# Guardo el archivo (con probas)
+# Guardo el archivo de probs
 archivo_salida <- paste0(PARAM$experimento, "_predicciones_semillas.csv")
 fwrite(predicciones, file = archivo_salida, sep = ",")
 
-#*********************************************GENERO ENSEMBLE*********************************************
-
-# Calcular el promedio de las predicciones (probas) de los 100 modelos ejecutados (excluyo cols "numero_de_cliente" y "foto_mes")
 predicciones$proba_ensemble <- rowMeans(predicciones[, .SD, .SDcols = -(1:2)])
 
 cat("\n\nEnsemble generado, hora:", Sys.time(), "\n")
-
-#*********************************************GENERO ENTREGA A KAGGLE*********************************************
 # Ordeno por probabilidad descendente
 setorder(predicciones, -proba_ensemble)
-
-
 # Genero archivos variando la cantidad de estímulos
 cortes <- seq(8000, 15000, by = 500)
 for (envios in cortes) {
